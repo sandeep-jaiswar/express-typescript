774adce1798461af253763fd7ebb69addb46b803
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2022-11-05 13:23:21 +0530

    day 2: MongoDB, models and environment variables

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,35 +1,68 @@
 import * as express from 'express';
 import Post from './post.interface';
+import PostModel from './posts.model';
 
 class PostsController {
 	public path = '/posts';
+	public pathById = '/posts/:id';
 	public router = express.Router();
-
-	private posts: Post[] = [
-		{
-			author: 'Marcin',
-			content: 'Dolor sit amet',
-			title: 'Lorem Ipsum',
-		},
-	];
 
 	constructor() {
 		this.intializeRoutes();
 	}
 
-	public intializeRoutes() {
+	private intializeRoutes() {
 		this.router.get(this.path, this.getAllPosts);
+		this.router.get(this.pathById, this.getPostById);
 		this.router.post(this.path, this.createAPost);
+		this.router.patch(this.pathById, this.modifyPost);
+		this.router.delete(this.pathById, this.deletePost);
 	}
 
-	getAllPosts = (request: express.Request, response: express.Response) => {
-		response.send(this.posts);
+	private getAllPosts = async (
+		request: express.Request,
+		response: express.Response,
+	) => {
+		const allPosts = await PostModel.find();
+		response.send(allPosts);
 	};
 
-	createAPost = (request: express.Request, response: express.Response) => {
-		const post: Post = request.body;
-		this.posts.push(post);
+	private createAPost = async (
+		request: express.Request,
+		response: express.Response,
+	) => {
+		const postData: Post = request.body;
+		const createdPost = new PostModel(postData);
+		const savedPost = await createdPost.save();
+		response.send(savedPost);
+	};
+
+	private getPostById = async (
+		request: express.Request,
+		response: express.Response,
+	) => {
+		const {id} = request.params;
+		const post = await PostModel.findById(id);
 		response.send(post);
+	};
+
+	private modifyPost = async (
+		request: express.Request,
+		response: express.Response,
+	) => {
+		const {id} = request.params;
+		const postData: Post = request.body;
+		const post = await PostModel.findByIdAndUpdate(id, postData, {new: true});
+		response.send(post);
+	};
+
+	private deletePost = async (
+		request: express.Request,
+		response: express.Response,
+	) => {
+		const {id} = request.params;
+		await PostModel.findByIdAndDelete(id);
+		response.send(200);
 	};
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,39 @@
+import 'dotenv/config';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import * as mongoose from 'mongoose';
+import App from './app';
+import PostsController from 'posts/posts.controller';
 
 const router = express.Router();
+const {MONGO_USER, MONGO_PASSWORD, MONGO_PATH} = process.env;
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
 
-function loggerMiddleware(
-	request: express.Request,
-	response: express.Response,
-	next,
-) {
-	console.log(`${request.method} ${request.path}`);
-	next();
-}
+// function loggerMiddleware(
+// 	request: express.Request,
+// 	response: express.Response,
+// 	next,
+// ) {
+// 	console.log(`${request.method} ${request.path}`);
+// 	next();
+// }
 
-const app = express();
+const app = new App([new PostsController()]);
 
 router.get('/hello', (request, response) => {
 	response.send('Hello world!');
 });
 
-app.use('/api', router);
+// app.use('/api', router);
 
-app.use(loggerMiddleware);
-app.use(bodyParser.json());
+// app.use(loggerMiddleware);
 
-app.get('/', (request, response) => {
-	response.send({
-		hostname: request.hostname,
-		path: request.path,
-		method: request.method,
-	});
-});
+// app.get('/', (request, response) => {
+// 	response.send({
+// 		hostname: request.hostname,
+// 		path: request.path,
+// 		method: request.method,
+// 	});
+// });
 
-app.listen(5000);
+app.listen();
